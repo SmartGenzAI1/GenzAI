1ca47fd4c17bf3c1cd0f4b8820293adb11c60469
Saff9 <saffanakbar942@gmail.com>
2025-10-23 11:57:28 +0530

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,12 @@
+// frontend/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'],
+  },
+}
+
+module.exports = nextConfig
